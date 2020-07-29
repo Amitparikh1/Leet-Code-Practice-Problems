affaaf29e9ecae1f09684fa143f73b4b77ec1108
Amit Parikh <amitparikh41@gmail.com>
2020-07-29 19:49:54 -0400

    finished solution

--- Arrays101/DuplicateZeros.java
+++ Arrays101/DuplicateZeros.java
@@ -1,15 +1,34 @@
+/*Problem: "Given a fixed length array arr of integers, duplicate each occurrence of zero, shifting the remaining elements to the right.
+
+Note that elements beyond the length of the original array are not written.
+
+Do the above modifications to the input array in place, do not return anything from your function."
+*/
 class Solution {
     public void duplicateZeros(int[] arr) {
         int numDuplicated = 0;
-        
+        int length = arr.length-1;
         //Find number of duplicated zeros
-        for (int i = 0; i<arr.length-numDuplicated;i++){
+        for (int i = 0; i<=length - numDuplicated;i++){
             if (arr[i]==0){
                 //edge case
-                if (i==arr.length-numDuplicated-1){
-                    
+                if (i==length - numDuplicated){
+                    arr[length] = 0;
+                    length--;
+                    break; //this would be the last 0 only
                 }
                 numDuplicated++;
+            }
+        }
+        //Shift positions
+        for (int i = length - numDuplicated;i>=0;i--){
+            if (arr[i]==0){
+                arr[i+numDuplicated]=0;
+                numDuplicated--;
+                arr[i+numDuplicated]=0;
+            }
+            else {
+                arr[i+numDuplicated]=arr[i];
             }
         }
         return;
